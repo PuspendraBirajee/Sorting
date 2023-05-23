9e00b923c4275bbe38d5a1de741a2b96f132d6bd
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-23 11:55:44 +0530

    Add files via upload

---
+++ Selection sort.cpp
@@ -0,0 +1,38 @@
+//2.3) Selection sort, to sort a given list of integers. 
+//Program: 
+#include<stdio.h> 
+int max(int *arr,int i) 
+{ 
+ int j,maxx=0,k; 
+ for(j=i;j>=0;j--) 
+{ 
+  if(arr[j]>maxx) 
+{ 
+   maxx=arr[j]; 
+   k=j; 
+  } 
+ } 
+ return k; 
+} 
+void selection_sort(int *arr,int n) 
+{ 
+ int i,l,k; 
+ for(i=n-1;i>=0;i--) 
+{ 
+  l=max(arr,i); 
+  k=arr[l]; 
+  arr[l]=arr[i]; 
+  arr[i]=k; 
+ } 
+} 
+int main() 
+{ 
+ int n,i,j,k,l; 
+ scanf("%d",&n); 
+ int arr[n]; 
+ for(i=0;i<n;i++) 
+  scanf("%d",&arr[i]); 
+ selection_sort(arr,n); 
+ for(i=0;i<n;i++) 
+  printf("%d ",arr[i]); 
+} 
